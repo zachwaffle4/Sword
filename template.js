6326670036bf01e379b7b5f174f298f928b4ef01
admiralwaffle4 <56054320+admiralwaffle4@users.noreply.github.com>
2020-07-18 13:51:11 -0400

    Create template.js

---
+++ template.js
@@ -0,0 +1,13 @@
+//New Command Template:
+
+const Discord = require('discord.js');
+
+module.exports = {
+	name: '',
+	description: '',
+	aliases: [],
+	usage: [],
+	async execute(message, args) {
+
+	},
+};
